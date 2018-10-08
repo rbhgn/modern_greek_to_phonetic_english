43f5bb50bf0d0ec8aab5f285cd5e3e524d8b34ce
rbhgn <robhgn@gmail.com>
2018-10-08 12:05:27 +0200

    Added combinedDigraphs function

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { greekAlphabet } from './greekAlphabet'
-import { alphabetChars, alphabetDigraphs } from './translator'
+import { getAlphabetChars, getAlphabetDigraphs, combineDigraphs } from './translator'
 
 
 
 
-test('makeAlphabetCharacters', () =>{
-  const value = alphabetChars(greekAlphabet)
+test('getAlphabetCharacters', () =>{
+  const value = getAlphabetChars(greekAlphabet)
   expect(value.length).toBe(31)
   expect(value.includes('γ')).toBe(true)
   expect(value.includes('a')).toBe(false)
 })
 
-test('makeAlphabetDigraphs', () =>{
-  const value = alphabetDigraphs(greekAlphabet)
+test('getAlphabetDigraphs', () =>{
+  const value = getAlphabetDigraphs(greekAlphabet)
   expect(value.length).toBe(8)
   expect(value.includes('γγ')).toBe(true)
   expect(value.includes('γ')).toBe(false)
+})
+
+test('getCombineDigraphs', () =>{
+  console.log(combineDigraphs('γγγ', greekAlphabet))
+  expect(combineDigraphs('', greekAlphabet)).toMatchObject([])
+  expect(combineDigraphs('αβ', greekAlphabet)).toMatchObject(['α', 'β'])
+  expect(combineDigraphs('αμπχ', greekAlphabet)).toMatchObject(['α', 'μπ', 'χ'])
+  expect(combineDigraphs('αυευ', greekAlphabet)).toMatchObject(['αυ', 'ευ'])
+  expect(combineDigraphs('γγγ', greekAlphabet)).toBe(['γγ', 'γ'])
 })
--- src/translator.js
+++ src/translator.js
@@ -2,10 +2,21 @@ import { greekAlphabet } from './greekAlphabet'
 
 
 
-export const alphabetChars = (greekAlphabet) => {
+export const getAlphabetChars = (greekAlphabet) => {
   return greekAlphabet.map(a => a.chars).reduce((a, b) => a.concat(b))
 }
 
-export const alphabetDigraphs = (greekAlphabet) => {
-  return greekAlphabet.map(a => a.chars).reduce((a, b) => a.concat(b)).filter(c => c.length > 1)
+export const getAlphabetDigraphs = (alphabet) => {
+  return alphabet.map(a => a.chars).reduce((a, b) => a.concat(b)).filter(c => c.length > 1)
+}
+
+export const combineDigraphs = (input, alphabet) => {
+  const digraphs = getAlphabetDigraphs(alphabet)
+  const inputArray = input.split('')
+  const result = inputArray.map((c, i, a) => {
+    if (i > 0 && digraphs.includes(a[i - 1] + c)) return false
+    if (i < a.length - 1 && digraphs.includes(c + a[i + 1])) return c + a[i + 1]
+    return c
+  })
+  return result
 }
