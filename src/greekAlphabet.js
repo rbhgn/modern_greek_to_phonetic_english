f26206f768932d22bef6116fc97d51466eb13623
rbhgn <robhgn@gmail.com>
2018-10-08 09:13:00 +0200

    Sorted the alphabet

---
+++ src/greekAlphabet.js
@@ -0,0 +1,56 @@
+
+
+// α a in "spa"
+// β v in "very"
+// ζ z in "zoo"
+// κ k in "kick"
+// λ l in "lamb"
+// ξ x in "vex"
+// ο o in "smoke"
+// θ th in "thing"
+// π p in "pear" or "Peter"
+// τ t in "tan"
+// ω o in "smoke"
+// ψ ps in "lips"
+// σ/ς s in "signal"
+
+// δ th in "the" or "this"
+// μ m in "mother" or "mabel"
+// ν n in "never" or "November"
+// φ f in "friday" or "fun"
+
+// ει ee in "bee" or e in "me"
+// η ee in "bee" or e in "me"
+// ι ee in "bee" or e in "me"
+// υ ee in "bee" or e in "me"
+
+// γγ ng in "finger" before κ, χ, ξ: nasal "ng" in "singer"
+
+// ου ou in "soup" or oo in "shoot"
+
+// ε e in "best" sometimes ay in "may"
+// αι e in "best" at the end of a word: ay in "may"
+
+// γ guttural g (no English equivalent) before an "e" or "i" sound, pronounced as y in "yes"
+// χ no English equivalent, rough, throaty sound similar to ch in "loch"
+// ρ rolled r (no English equivalent - the sound is like the rolled r in the italian or Spanish language
+
+// αυ before vowels and voiced consonants: "av" in "avatar" anywhere else: "af" in "affordable"
+// ευ before vowels and voiced consonants: "ev" in "event" anywhere else: "ef" in "effort"
+
+// ντ middle of words: "nd" in "hand" beginning of words: "d" in "dog" 
+// μπ middle of words: "mb" in "dumb" beginning of words: "b" in "best" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
