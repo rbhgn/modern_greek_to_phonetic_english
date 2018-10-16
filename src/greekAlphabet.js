35ce841a6be2d5536895b0d4890850da9c3b0e2d
rbhgn <robhgn@gmail.com>
2018-10-16 17:24:14 +0200

    Added Accented Letters

--- src/greekAlphabet.js
+++ src/greekAlphabet.js
@@ -1,11 +1,11 @@
 export const greekAlphabet = 
 [
-  {chars: ['α']                , default: {char: 'a' , vowel: true }},
+  {chars: ['α', 'ά']                , default: {char: 'a' , vowel: true }},
   {chars: ['β']                , default: {char: 'v' , vowel: true }},
   {chars: ['ζ']                , default: {char: 'z' , vowel: true }},
   {chars: ['λ']                , default: {char: 'l' , vowel: true }},
   {chars: ['ξ']                , default: {char: 'x' , vowel: false}},
-  {chars: ['ο', 'ω']           , default: {char: 'o' , vowel: true }},
+  {chars: ['ο', 'ω', 'ό', 'ώ']           , default: {char: 'o' , vowel: true }},
   {chars: ['θ', 'δ']           , default: {char: 'th', vowel: true }},
   {chars: ['π']                , default: {char: 'p' , vowel: false}},
   {chars: ['τ']                , default: {char: 't' , vowel: false}},
@@ -13,10 +13,10 @@ export const greekAlphabet =
   {chars: ['ν']                , default: {char: 'n' , vowel: true }},
   {chars: ['φ']                , default: {char: 'f' , vowel: false}},
   {chars: ['σ', 'ς']           , default: {char: 's' , vowel: false}},
-  {chars: ['ει', 'η', 'ι', 'υ'], default: {char: 'ee', vowel: true } , EI: true},
+  {chars: ['ει', 'η', 'ι', 'υ', 'ϊ', 'ΐ', 'ύ', 'ϋ', 'ΰ', 'ή'], default: {char: 'ee', vowel: true } , EI: true},
   {chars: ['γγ']               , default: {char: 'ng', vowel: true }},
   {chars: ['ου']               , default: {char: 'ou', vowel: true }},
-  {chars: ['αι', 'ε']          , default: {char: 'e' , vowel: true}  , endOfWord:   {char: 'ay', vowel: true } , EI: true},
+  {chars: ['αι', 'ε', 'έ']          , default: {char: 'e' , vowel: true}  , endOfWord:   {char: 'ay', vowel: true } , EI: true},
   {chars: ['αυ']               , default: {char: 'af', vowel: true}  , beforeVowel: {char: 'av', vowel: true }},
   {chars: ['ευ']               , default: {char: 'ef', vowel: true}  , beforeVowel: {char: 'ev', vowel: true }},
   {chars: ['ντ']               , default: {char: 'nd', vowel: true}  , startOfWord: {char: 'd' , vowel: true }},
@@ -25,3 +25,12 @@ export const greekAlphabet =
   {chars: ['χ']                , default: {char: 'ch', vowel: false}},
   {chars: ['ρ']                , default: {char: 'r' , vowel: true }}
 ]
+
+
+
+
+
+
+
+
+
